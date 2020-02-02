8e291c38cc2d9f4a26873aa1cc3da34225669af0
Andy Swick <andymswick@gmail.com>
2020-02-02 12:11:41 -0600

    Styling

--- src/ExpoServiceProvider.php
+++ src/ExpoServiceProvider.php
@@ -12,12 +12,12 @@ class ExpoServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../config/expo.php' => config_path('expo.php'),
+            __DIR__.'/../config/expo.php' => config_path('expo.php'),
         ], 'config');
 
-        if (!class_exists('AddPushTokenToUsersTable')) {
+        if (! class_exists('AddPushTokenToUsersTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/add_push_token_to_users_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_add_push_token_to_users_table.php'),
+                __DIR__.'/../database/migrations/add_push_token_to_users_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_add_push_token_to_users_table.php'),
             ], 'migrations');
         }
     }
@@ -27,6 +27,6 @@ class ExpoServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/expo.php', 'expo');
+        $this->mergeConfigFrom(__DIR__.'/../config/expo.php', 'expo');
     }
 }
