3c807d073e962396416f7b41823a2d5e9705894d
Andy Swick <andreamswick@gmail.com>
2020-02-02 06:12:00 +0000

    Apply fixes from StyleCI

--- src/ExpoServiceProvider.php
+++ src/ExpoServiceProvider.php
@@ -15,18 +15,18 @@ class ExpoServiceProvider extends ServiceProvider
 
         /**
          * Here's some example code we use for the pusher package.
-
-        $this->app->when(Channel::class)
-            ->needs(Pusher::class)
-            ->give(function () {
-                $pusherConfig = config('broadcasting.connections.pusher');
-
-                return new Pusher(
-                    $pusherConfig['key'],
-                    $pusherConfig['secret'],
-                    $pusherConfig['app_id']
-                );
-            });
+         *
+         * $this->app->when(Channel::class)
+         * ->needs(Pusher::class)
+         * ->give(function () {
+         * $pusherConfig = config('broadcasting.connections.pusher');
+         *
+         * return new Pusher(
+         * $pusherConfig['key'],
+         * $pusherConfig['secret'],
+         * $pusherConfig['app_id']
+         * );
+         * });
          */
     }
 
